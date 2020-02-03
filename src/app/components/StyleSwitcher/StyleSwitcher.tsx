bc1e90a1a5c8c19cdcdfb27ec4a1758fc74a8f7d
jtinianow <jermtee@gmail.com>
2020-02-03 14:44:30 -0700

    make ui match figma closer

--- src/app/components/StyleSwitcher/StyleSwitcher.tsx
+++ src/app/components/StyleSwitcher/StyleSwitcher.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import cx from 'classnames'
 
 interface StyleSwitcherProps {
     styles: {
@@ -17,19 +18,56 @@ interface StyleSwitcherProps {
 
 const StyleSwitcher = (props: StyleSwitcherProps) => {
     const { styles, currentStyle, setBaseFontStyle, baseFontName } = props;
-    console.log({ currentStyle })
+
+    const [showOptions, toggleOptions] = React.useState(false)
+    console.log({ showOptions })
 
     return (
         <div>
-            <select defaultValue={currentStyle} onChange={e => setBaseFontStyle({ style: e.target.value, family: baseFontName.family })}>
+            <button
+                className="select-menu__button"
+                onClick={() => toggleOptions(true)}
+                type="button"
+            >
+                {currentStyle}
+            </button>
+            {
+                showOptions &&
+                <ul className="select-menu__list select-menu__list--active">
+                    {styles.map(style => (
+                        <li
+                            onClick={() => {
+                                setBaseFontStyle({ style: style.fontName.style, family: style.fontName.family })
+                                toggleOptions(false)
+                            }}
+                            className={cx('select-menu__list-item', {
+                                "select-menu__list-item--active": style.fontName.style === currentStyle
+                            })}
+                            key={style.fontName.style}
+                        >
+                            <span className="select-menu__list-item-icon" />
+                            <span className="select-menu__list-item-text">
+                                {style.fontName.style}
+                            </span>
+                        </li>
+                    ))}
+                </ul>
+            }
+            {/* <select
+                name="fontStyle"
+                id="fontStyle"
+                defaultValue={currentStyle}
+                onChange={e => setBaseFontStyle({ style: e.target.value, family: baseFontName.family })}
+                className="select-menu select-menu__button"
+            >
                 {styles.map(style => (
                     <option
                         key={style.fontName.style}
                         value={style.fontName.style}
                     >{style.fontName.style}</option>
                 ))}
-            </select>
-        </div>
+            </select> */}
+        </div >
     )
 }
 
